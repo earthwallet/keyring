fcc6f61322964a5b4ff0830f95a5827917f25d3e
earthgohan <85003405+earthgohan@users.noreply.github.com>
2021-10-10 18:09:13 +0530

    Upgrade dfinity packages and migrate NFT func

--- src/util/icp/index.spec.ts
+++ src/util/icp/index.spec.ts
@@ -2,13 +2,7 @@
 import test from 'ava';
 
 import { createWallet } from '../../lib/wallet';
-import {
-  sendICP,
-  indexToHash,
-  getNFTsFromCanisterExt,
-  transferNFTsExt,
-  listNFTsExt,
-} from '.';
+import { sendICP, indexToHash } from '.';
 
 //https://github.com/dfinity/internet-identity/tree/main
 
@@ -40,73 +34,6 @@ test('get hash from index with indexToHash', async (t) => {
       hash,
       'dad49f3a954a4109410b4d7af65f3e6385e4dc35872c3c21ed1d0135fe27e52e'
     );
-  } catch (error) {
-    console.log(error);
-    t.truthy(false);
-  }
-});
-
-test('get tokens for a canister for a user', async (t) => {
-  try {
-    const tokens = await getNFTsFromCanisterExt(
-      'owuqd-dyaaa-aaaah-qapxq-cai',
-      '0ba1b7b1643929210dc41a8afbe031bd1b5e81dbc8e3b3b64978f5f743f058c3'
-    );
-
-    t.like(tokens[0], {
-      metadata: [],
-      info: {
-        seller:
-          'o7nwu-n6kuf-4afzp-ybcuf-346pr-odd54-damf5-v4pvc-4sexh-cabph-7qe',
-        price: BigInt(18000000000),
-        locked: [],
-      },
-      tokenIndex: 2112,
-      tokenIdentifier: '5pzgh-likor-uwiaa-aaaaa-b4ad5-4aqca-aabba-a',
-      forSale: true,
-    });
-  } catch (error) {
-    console.log(error);
-    t.truthy(false);
-  }
-});
-
-test('transfer saleable NFT of a canister should give TOKEN_LISTED_FOR_SALE status', async (t) => {
-  try {
-    const seedPhrase =
-      'open jelly jeans corn ketchup supreme brief element armed lens vault weather original scissors rug priority vicious lesson raven spot gossip powder person volcano';
-
-    const walletObj = await createWallet(seedPhrase, 'ICP');
-
-    const status = await transferNFTsExt(
-      'owuqd-dyaaa-aaaah-qapxq-cai',
-      walletObj.identity,
-      '0ba1b7b1643929210dc41a8afbe031bd1b5e81dbc8e3b3b64978f5f743f058c3',
-      '2112'
-    );
-
-    t.is(status, 'TOKEN_LISTED_FOR_SALE');
-  } catch (error) {
-    console.log(error);
-    t.truthy(false);
-  }
-});
-
-test('list not owned NFT of a canister should give UNAUTHORISED status', async (t) => {
-  try {
-    const seedPhrase =
-      'open jelly jeans corn ketchup supreme brief element armed lens vault weather original scissors rug priority vicious lesson raven spot gossip powder person volcano';
-
-    const walletObj = await createWallet(seedPhrase, 'ICP');
-
-    const status = await listNFTsExt(
-      'owuqd-dyaaa-aaaah-qapxq-cai',
-      walletObj.identity,
-      '2112',
-      4
-    );
-
-    t.is(status, 'UNAUTHORISED');
   } catch (error) {
     console.log(error);
     t.truthy(false);
